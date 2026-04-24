b7ab0f03ec37536d2b8c21d9c9a7a9410dd04308
Nestor0000 <silvamunoznestor@gmail.com>
2026-04-24 10:27:00 +0200

    he corregido lo de los grit

--- include/fondos.h
+++ include/fondos.h
@@ -4,6 +4,9 @@
 fondos.h
 -------------------------------------*/
 extern void visualizarfondofase1();
+extern void visualizarfondofase2();
+extern void visualizarfondofase3();
+extern void visualizarfondofase4();
 extern void visualizarPuerta();
 extern void visualizarPuertaAbierta();
 
