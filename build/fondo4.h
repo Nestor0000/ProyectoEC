7f5ff35aba9c503c700b2ca1920c526fe0a7e877
Nestor0000 <silvamunoznestor@gmail.com>
2026-04-30 22:55:52 +0200

    a

--- build/fondo4.h
+++ build/fondo4.h
@@ -8,7 +8,7 @@
 //	+ bitmap not compressed
 //	Total size: 98304 = 98304
 //
-//	Time-stamp: 2026-04-30, 16:42:07
+//	Time-stamp: 2026-04-30, 16:54:58
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
